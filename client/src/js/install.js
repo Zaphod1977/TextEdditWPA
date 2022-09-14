5fbeed1dbfea53c03ebba2da50b405817dd4eff2
carfiguru@hotmail.com <carfiguru@hotmail.com>
2022-09-14 09:38:26 -0500

    added todo functions

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -9,3 +9,22 @@ butInstall.addEventListener('click', async () => {});
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {});
+
+
+
+
+
+// window.addEventListener('beforeinstallprompt', (event) => {
+//     event.preventDefault();
+//     installBtn.style.visibility = 'visible';
+  
+//     installBtn.addEventListener('click', () => {
+//       event.prompt();
+//       installBtn.setAttribute('disabled', true);
+//       installBtn.textContent = 'Installed!';
+//     });
+//   });
+  
+//   window.addEventListener('appinstalled', (event) => {
+//     console.log('👍', 'appinstalled', event);
+//   });
