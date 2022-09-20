3e1ffe7423769471d3df44415184d47fe104b17d
carfiguru@hotmail.com <carfiguru@hotmail.com>
2022-09-19 19:56:07 -0500

    added node items

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,29 +2,28 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    butInstall.style.visibility = 'visible';
+  
+    butInstall.addEventListener('click', () => {
+      event.prompt();
+      butInstall.setAttribute('disabled', true);
+      butInstall.textContent = 'Installed!';
+    });
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async (event) => {
+    event.preventDefault();
+        // check if CodeMirror is loaded
+        if (typeof CodeMirror === 'undefined') {
+            throw new Error('CodeMirror is not loaded');
+          }
+          console.log(CodeMirror);
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
-
-
-
-
-
-// window.addEventListener('beforeinstallprompt', (event) => {
-//     event.preventDefault();
-//     installBtn.style.visibility = 'visible';
-  
-//     installBtn.addEventListener('click', () => {
-//       event.prompt();
-//       installBtn.setAttribute('disabled', true);
-//       installBtn.textContent = 'Installed!';
-//     });
-//   });
-  
-//   window.addEventListener('appinstalled', (event) => {
-//     console.log('👍', 'appinstalled', event);
-//   });
+window.addEventListener('appinstalled', (event) => {
+    console.log('👍', 'appinstalled', event);
+});
