b310c8e9728f88a37b9810102bf00bc682ef39bb
carfiguru@hotmail.com <carfiguru@hotmail.com>
2022-09-19 19:34:47 -0500

    database js TODO's configuration

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -32,12 +32,12 @@ module.exports = () => {
         publicPath: './',
         icons: [
           {
-            src: path.resolve('src/images/icon-manifest.png'),
+            src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
           },
           {
-            src: path.resolve('src/images/icon-manifest.png'),
+            src: path.resolve('src/images/logo.png'),
             size: '1024x1024',
             destination: path.join('assets', 'icons'),
             purpose: 'maskable'
